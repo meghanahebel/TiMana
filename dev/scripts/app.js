c36abf9452a4af94cee447092152f340d9690b43
Meghan Hebel <meghanrhebel@gmail.com>
2018-01-16 20:13:58 -0700

    form creation for login and firebase setup

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import { 
   BrowserRouter as Router, 
   Route, Link, NavLink, Switch } from 'react-router-dom';
@@ -7,6 +8,18 @@ import {
 import Home from './components/pages/Home';
 import Login from './components/pages/home/Login';
 
+// FIREBASE
+var config = {
+  apiKey: "AIzaSyCRusQmoJ1YbOKZ8g-jn5LJSwLBvft1M_A",
+  authDomain: "timana-b9f09.firebaseapp.com",
+  databaseURL: "https://timana-b9f09.firebaseio.com",
+  projectId: "timana-b9f09",
+  storageBucket: "timana-b9f09.appspot.com",
+  messagingSenderId: "937903541996"
+};
+firebase.initializeApp(config);
+
+// APP COMPONENT
 class App extends React.Component {
   constructor() {
     super();
@@ -23,6 +36,6 @@ class App extends React.Component {
       </Router>
     )
   }
-}
+} // end of App 
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- dev/scripts/components/pages/home/Login.js
+++ dev/scripts/components/pages/home/Login.js
@@ -2,9 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            createEmail: '',
+            createPassword: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.createUser = this.createUser.bind(this);
+    }
+
+    handleChange(e, field) {
+        console.log(e);
+        let newState = Object.assign({}, this.state);
+        newState[field] = e.target.value;
+        this.setState(newState);
+    } // end of handleChange - assigns input values to state dynamically
+ 
+    createUser(e) {
+        e.preventDefault();
+        let email = this.state.createEmail;
+        let password = this.state.createPassword;
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch((error) => console.log(error.code, error.message));
+    }
+
     render() {
         return (
-            <div>This is the Login Component</div>
+            <div>
+                <form action="">
+                    <label htmlFor="email">Email <span>(or username)</span></label>
+                    <input type="text" name="email" id="login_email" placeholder = "e.g. sherlock.holmes@baker.com" 
+                        onChange = {(e)=>this.handleChange(e, "createEmail")}/>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="login_password" placeholder = "e.g. sherlocked" 
+                        onChange = {(e)=>this.handleChange(e, "createPassword")}/>
+                    <button className="login_submit">Login</button>
+                </form>
+            </div>
         )
     }
 }
